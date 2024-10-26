cbaeda8cd48f29033b82a63a7eaf80227ef957bc
Pedro Henrique <155556494+Pehzinz@users.noreply.github.com>
2024-10-25 23:24:14 -0300

    Add files via upload

---
+++ POC_React/app/page.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import styles from '../styles/Home.module.css';
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Header />
+            <h2 className={styles.title}>Parte/pagina principal do codigo</h2>
+        </div>
+    );
+}
+//Pedro Henrique Mendes Vieira Gonçalves - RA: 10439486
+//Leonardo Luppi Vanni Valente - RA: 10436603
---
+++ POC_React/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header=()=> {
+    return (
+        <header>
+            <h1>POC_React, testando seus conceitos!</h1>
+        </header>
+    );
+};
+export default Header;
+//Pedro Henrique Mendes Vieira Gonçalves - RA: 10439486
+//Leonardo Luppi Vanni Valente - RA: 10436603
